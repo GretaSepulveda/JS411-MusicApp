72052259012eaa9f74c46a2f55a9cf82e5714c76
MariaGretaSepulveda <sepulveda.greta@gmail.com>
2019-09-25 19:47:59 -0500

    initial commit

---
+++ src/App.js
@@ -0,0 +1,53 @@
+import React, { Component } from 'react';
+import './App.css';
+import Login from './Component/Login'
+import Dashboard from './Component/Dashboard';
+import { AppBar, Toolbar, Typography } from '@material-ui/core';
+
+class App extends Component {
+
+  state = {
+    show: false
+  }
+
+  handleClick = () => {
+    this.setState({ show: !this.state.show })
+  }
+
+
+  render() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <AppBar position="sticky">
+          <Toolbar>
+            <Typography variant="h6">
+              Music App
+            </Typography>
+          </Toolbar>
+        </AppBar>
+      </header>
+      {this.state.show ? (
+        <div>
+        <Dashboard>
+        <button onClick={this.handleClick}></button>
+          Login
+        </Dashboard>
+        </div>
+      ) : (
+        <Login login={this.handleClick}/>
+
+      )
+      }
+    </div>
+  );
+  }
+}
+
+
+
+
+
+
+
+export default App;
